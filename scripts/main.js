5929257dd9cffa33c67e918c87b4312d17a3e2d8
Arlesson-Sales <urameshy11@gmail.com>
2025-01-22 23:29:29 -0300

    Arrumando bug nas imagens

--- scripts/main.js
+++ scripts/main.js
@@ -42,7 +42,7 @@ function createLinkSectionHTML({ data })
         html_section.innerHTML = `
             <header>
                 <div class="links-section-header">
-                    <div class="header-background" style="background-image: url(${section_data.background})">
+                    <div class="header-background" style="background-image: url(../${section_data.background})">
                         <div class="links-section-titles">
                             <h4>${section_data.title}</h4>
                             <h5>${section_data.sub_title}</h5>
